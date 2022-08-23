791fed550ca1645c9e790feb65d9376b0108be38
shahbazahmedt1999 <111744223+shahbazahmedt1999@users.noreply.github.com>
2022-08-23 11:51:53 +0530

    commit changes

--- ReverseString.java
+++ ReverseString.java
@@ -5,7 +5,7 @@ public class ReverseString {
         String s = "Tasmiya";
         for(int i= s.length()-1; i>=0; i--){
             System.out.println(s.charAt(i));
-			System.out.println("welcome to reverse edureka platform");
+		System.out.println("welcome to reverse edureka platform");
         }
     }
 }
