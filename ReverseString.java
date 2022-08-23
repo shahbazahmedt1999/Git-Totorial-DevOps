693f1ca8e13b1fc5254d9bb6e818fa0c8f42a69c
Admin of this PC <shahbazahmed7277@gmail.com>
2022-08-23 11:49:06 +0530

    new file

---
+++ ReverseString.java
@@ -0,0 +1,11 @@
+package com.company.program;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String s = "Tasmiya";
+        for(int i= s.length()-1; i>=0; i--){
+            System.out.println(s.charAt(i));
+			System.out.println("welcome to reverse edureka platform");
+        }
+    }
+}
